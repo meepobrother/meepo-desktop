ce2fd39a81d19b99edf2eb3ef2aad87298f5b3e3
15138730370 <1037483576@qq.com>
2017-11-28 21:11:26 +0800

    save

--- app/javascripts/main/index.js
+++ app/javascripts/main/index.js
@@ -6,27 +6,27 @@ json = require('../../package.json');
 electron = require('electron');
 
 electron.app.on('ready', function() {
-  var window;
+    var window;
 
-  window = new electron.BrowserWindow({
-    title: json.name,
-    width: json.settings.width,
-    height: json.settings.height
-  });
-
-  window.loadURL('file://' + path.join(__dirname, '..', '..') + '/index.html');
-
-  window.webContents.on('did-finish-load', function(){
-    window.webContents.send('loaded', {
-      appName: json.name,
-      electronVersion: process.versions.electron,
-      nodeVersion: process.versions.node,
-      chromiumVersion: process.versions.chrome
+    window = new electron.BrowserWindow({
+        title: json.name,
+        width: json.settings.width,
+        height: json.settings.height
     });
-  });
 
-  window.on('closed', function() {
-    window = null;
-  });
+    window.loadURL('file://' + path.join(__dirname, '..', '..') + '/index.html');
+
+    window.webContents.on('did-finish-load', function() {
+        window.webContents.send('loaded', {
+            appName: json.name,
+            electronVersion: process.versions.electron,
+            nodeVersion: process.versions.node,
+            chromiumVersion: process.versions.chrome
+        });
+    });
+
+    window.on('closed', function() {
+        window = null;
+    });
 
 });
